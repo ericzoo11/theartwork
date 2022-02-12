53f5e0f66ae83580575097fbbd6eab8c12643061
Eric Zhu <ericzhu@Erics-MacBook-Air.local>
2022-02-12 15:37:37 -0500

    Modals working for each image

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -81,16 +81,25 @@ function Submit() {
 }
 // Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("disneyImage");
+var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+
+document.addEventListener('click', function(e) {
+    var targetId = e.target.id;
+    //simple id filter
+    if(targetId == "myImg" || targetId == "myImg1" || targetId == "myImg2" || targetId == "myImg3"){
+        img = document.getElementById(e.target.id);
+        console.log("img")
+        modal.style.display = "block";
+        modalImg.src = img.src;
+        captionText.innerHTML = img.alt;
+    }
+}, false);
+
+
+
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
